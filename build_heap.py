e3f34ec02838b25ee10619b9be9f1c73b900ec3b
Linards Beķeris <99728928+221RDB161@users.noreply.github.com>
2023-03-19 02:30:55 +0000

    221RDB161(1meginajums)

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,92 @@
 # python3
-
+# Linards Tomass Beķeris 221RDB161 10.grupa
 
 def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    swaps = []  # swaps liste tuksha
+    n = len(data)  # ievades datu garums
 
+    # (n // 2 - 1)  0, -1
+    for i in range(n // 2 - 1, -1, -1):
+        swaps = sift_down(data, i, swaps) 
 
-    return swaps
+    return swaps  #atgriezh swaps
+
+def sift_down(data, i, swaps):
+    n = len(data)  # garums
+    minimalaisIndx = i  # minimalais index ka index i
+
+    kreisais = 2 * i + 1  # izrekina index
+    # parbauda vai eksiste
+    if kreisais < n and data[kreisais] < data[minimalaisIndx]:
+        minimalaisIndx = kreisais  # updato
+
+    labais = 2 * i + 2  # izrekina
+    # parbauda vai eksiste
+    if labais < n and data[labais] < data[minimalaisIndx]:
+        minimalaisIndx = labais  # updeito
+
+    # ja indekss no minimala elementa ir nomainijies no esosa i indeksa
+    if i != minimalaisIndx:
+        swaps.append((i, minimalaisIndx)) 
+        data[i], data[minimalaisIndx] = data[minimalaisIndx], data[i]
+        swaps = sift_down(data, minimalaisIndx, swaps)
+
+    return swaps  # atgriezh swaps
+
+def read_input_from_file(file_path):
+    try:
+        with open(f"tests/{file_path}", "r") as f:
+            text = f.read().strip()
+        return text.split('\n')
+    except FileNotFoundError:
+        print("Neatbilstošs faila path")
+        return None
+    except:
+        print("Kļūda nolasot failu")
+        return None
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    input_veids = input(
+        "Ievadi 'F' lai nolasītu inputu no faila, vai arī 'I' lai nolasītu input no klaviatūras: ").strip()
 
+    if input_veids == 'F':
+        file_name = input(
+            "Ievadi faila nosaukumu. (Tie faila nosaukumi kuros būs burts 'a' nedarbosies: ")
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        if 'a' in file_name:
+            print("Neatbilstošs faila nosaukums")
+            return
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+        file_path = f"{file_name.zfill(2)}"
+        input_lines = read_input_from_file(file_path)
 
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
+        if input_lines:
+            n = int(input_lines[0])
+            data = list(map(int, input_lines[1].split()))
+            assert len(data) == n 
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+            swaps = build_heap(data)
 
+            print(len(swaps)) 
 
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    elif input_veids == 'I':
+        try:
+            n = int(input())  
+            data = list(map(int, input("Ievade: ").split())) 
 
+            assert len(data) == n 
 
+            swaps = build_heap(data)  
+
+            print(len(swaps)) 
+            
+            for i, j in swaps:
+                print(i, j)
+
+        except:
+            print("Neatbilstoša ievade")
+    else:
+        print("Neatbilstošs ievades veids")
 if __name__ == "__main__":
     main()
